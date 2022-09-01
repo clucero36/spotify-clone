bf4e6b01db53af264ad474cb462c40cad0ddec43
Luis Lucero <clucero3696@gmail.com>
2022-08-31 21:28:21 -0700

    Cloud Functions Not Deploying

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,45 +1,45 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const querystring = require('query-string');
-const axios = require('axios').default;
 admin.initializeApp();
+const querystring = require('query-string');
 
 
-exports.userAuth = functions.https.onRequest((req, res) => {
+
+exports.requestSpotifyUserAuth = functions.https.onRequest((req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  const redirect_uri = 'http://localhost:5000/callback'
+  const redirect_uri = 'https://llucero.netlify.app';
   const scope = 'user-read-private user-read-email user-top-read user-read-recently-played playlist-read-private user-library-read';
   let qString = querystring.stringify({
     response_type: 'code',
-    client_id: "f7601cac702b49db92c02678ab6c5177",
+    client_id: functions.config().spotify.client_id,
     scope: scope,
     redirect_uri: redirect_uri,
-  })
+  });
+  res.send('https://accounts.spotify.com/authorize?' + qString);
+});
 
-  res.send({authUrl: 'https://accounts.spotify.com/authorize?' + qString});
-})
 
-
-exports.callBack = functions.https.onRequest((req, res) => {
+exports.spotifyAccessTokenCB = functions.https.onRequest((req, res) => {
+  const axios = require('axios').default;
   res.set('Access-Control-Allow-Origin', '*');
-  const redirect_uri = 'http://localhost:5000/callback';
+  const redirect_uri = 'https://spotifyditto.netlify.app/callback';
   const code = req.query.code;
-  
-  const buf = Buffer.from('f7601cac702b49db92c02678ab6c5177' + ':' + '78ba61007cf64462aa84551f740d6202', 'utf8')
-  const qString = querystring.stringify({
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirect_uri
-  })
-  console.log('https://accounts.spotify.com/api/token?' + qString);
-  axios.post('https://accounts.spotify.com/api/token?'+ qString, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + buf.toString('base64'),
-    }
-  }).then((x) => {
-    let params = {access_token: x.access_token, token_type:x.token_type}
-    res.send({token: 'https://spotifyditto.netlify.app/user?' + querystring.stringify(params)});
+
+  async function getToken(code) {
+    const buf = Buffer.from('f7601cac702b49db92c02678ab6c5177' + ':' + '78ba61007cf64462aa84551f740d6202', 'utf8')
+    const qString = querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirect_uri
+    })
+    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    axios.defaults.headers.post['Authorization'] = 'Basic ' + buf.toString('base64');
+    const response = await axios.post('https://accounts.spotify.com/api/token?'+ qString)
+    return response;
+  }
+  getToken(code).then((x) => {
+    let params = {access_token: x.data.access_token, token_type:x.data.token_type}
+    res.redirect('https://spotifyditto.netlify.app/user?' + querystring.stringify(params));
   }).catch(function (error) {
     console.log(error);
   })
