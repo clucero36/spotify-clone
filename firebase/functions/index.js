2a9ebf3ba0518933e91aa405596b650e14c70af0
llucero <clucero3696@gmail.com>
2023-05-22 16:15:59 -0700

    Added Cloud Function For Non Auth

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const querystring = require('query-string');
 
+// used for Spotify OAuth2.0 
 const redirect_uri = 'https://us-central1-spotify-82254.cloudfunctions.net/spotifyAccessTokenCB';
 exports.requestSpotifyUserAuth = functions.https.onRequest((req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -41,6 +42,29 @@ exports.spotifyAccessTokenCB = functions.https.onRequest((req, res) => {
     res.send(error);
   })
 })
+
+// for non auth users
+exports.clientCredentialsAuth = functions.https.onRequest((req, res) => {
+  const axios = require('axios').default;
+  res.set('Access-Control-Allow-Origin', '*');
+
+  async function getToken() {
+    const buf = Buffer.from(functions.config().spotify.client_id + ':' + functions.config().spotify.client_secret, 'utf8')
+    const qString = querystring.stringify({
+      grant_type: 'client_credentials',
+    })
+    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    axios.defaults.headers.post['Authorization'] = 'Basic ' + buf.toString('base64');
+    const response = await axios.post('https://accounts.spotify.com/api/token?'+ qString);
+    return await response.data;
+  }
+  getToken().then((x) => {
+    let params = {access_token: x.access_token, token_type:x.token_type}
+    res.send({url: 'https://spotifyditto.netlify.app/non-auth-callback?' + querystring.stringify(params)});
+  }).catch(function (error) {
+    res.send(error);
+  })
+});
 
 // http://localhost:3000
 // https://spotifyditto.netlify.app
