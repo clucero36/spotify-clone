2cfb2baa98acfa8dc457abd92b43c5ccaea4b45e
llucero <clucero3696@gmail.com>
2023-05-22 17:01:37 -0700

    URL Login Fix

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -16,14 +16,14 @@ const Login = () => {
   }
 
   async function nonAuth() {
-    axios.get('// https://us-central1-spotify-82254.cloudfunctions.net/clientCredentialsAuth').then((x) => {
+    axios.get('https://us-central1-spotify-82254.cloudfunctions.net/clientCredentialsAuth').then((x) => {
       window.location.href=x.data.url;
     })
   }
 
   return (
-    <Box align='center' m='4rem auto' w='50%'>
-      <Text fontSize='xl' p='5rem'>Spotify Clone</Text>
+    <Box align='center' m='4rem auto' w='80%'>
+      <Text fontSize='xl' p={['2.5rem', '5rem']}>Spotify Clone</Text>
       <Text>"Standard User" showcases a clone of the spotify webplayer @ https://open.spotify.com. </Text>
       <Text >The app is still in development. I'm currently exploring nested routes and reusability. </Text>
       <Button onClick={nonAuth} m='2rem 0'>Standard User</Button>
