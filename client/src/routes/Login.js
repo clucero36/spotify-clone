610273af3415724e6014e1d79702df56318be368
llucero <clucero3696@gmail.com>
2024-09-24 18:29:45 -0700

    Login Text

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
   }
 
   async function nonAuth() {
-    axios.get('https://us-central1-spotify-82254.cloudfunctions.net/clientCredentialsAuth').then((x) => {
+    axios.get('http://127.0.0.1:5001/spotify-82254/us-central1/clientCredentialsAuth').then((x) => {
       window.location.href=x.data.url;
     })
   }
@@ -25,11 +25,9 @@ const Login = () => {
     <Box align='center' m='4rem auto' w='80%'>
       <Text fontSize='xl' p={['2.5rem', '5rem']}>Spotify Clone</Text>
       <Text>"Standard User" showcases a clone of the spotify webplayer @ https://open.spotify.com. </Text>
-      <Text >The app is still in development. I'm currently exploring nested routes and reusability. </Text>
       <Button onClick={nonAuth} m='2rem 0'>Standard User</Button>
       <Divider />
-      <Text mt='2rem'>"Request Auth" also showcases a clone of the webplayer, however it utilizes Spotify's</Text>
-      <Text> implementation of OAuth2.0 and is available to those I personally give access to. </Text>
+      <Text mt='2rem'>"Request Auth" also showcases a clone of the webplayer, however it utilizes Spotify's implementation of OAuth2.0</Text>
       <Button onClick={reqAuth} m='2rem 0'>Authenticated User</Button>
     </Box>
   )
